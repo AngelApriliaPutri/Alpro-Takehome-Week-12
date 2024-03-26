ff56a8bb1d7acdf3ba5486ec7e701d58cbea2db7
AngelApriliaPutri <164986194+AngelApriliaPutri@users.noreply.github.com>
2024-03-26 12:42:30 +0700

    Add files via upload

---
+++ W12/W12/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int rek = 0;
+        long saldoawal = 0;
+        string n = "";
+        List<rekening> nrek = new List<rekening>();
+        List<string> urutrek = new List<string>();
+        string namarek = "";
+        int a = 0;
+        string pilih = "";
+        string pilih2 = "";
+        int setor = 0;
+        int tarik = 0;
+        long setoran = 0;
+        long tarikan = 0;
+        while (a == 0)
+        {
+            Console.WriteLine("|N| Tambah Rekening \n|X| Exit");
+            Console.WriteLine();
+            Console.WriteLine("Daftar Rekening: ");
+            if (rek == 0)
+            {
+                Console.WriteLine("Belum ada rekening \n");
+            }
+            if (rek >= 1)
+            {
+                for (int i = 0; i < rek; i++)
+                {
+                    Console.WriteLine("[" + urutrek[i] + "] " + nrek[i].nama);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pilih: ");
+            pilih = Console.ReadLine();
+            Console.WriteLine();
+            if (pilih == "N")
+            {
+                Console.WriteLine("Nama Rekening : ");
+                namarek = Console.ReadLine();
+                nrek.Add(new rekening( namarek, saldoawal));
+                rek++;
+                urutrek.Add("R"+rek);
+                Console.Clear();
+            }
+            else if (pilih == "X")
+            {
+                a++;
+            }
+            for (int i = 0; i < rek; i++)
+            {
+                if (pilih == urutrek[i])
+                {
+                    pilih2 = "";
+                    while (pilih2 != "X")
+                    {
+                        pilih = "O";
+                        Console.Clear();
+                        Console.WriteLine("REKENING " + nrek[i].nama);
+                        nrek[i].ceksaldo();
+                        Console.WriteLine();
+                        Console.WriteLine("[S] Setor \n[T] Tarik \n[X] Return");
+                        Console.Write("Pilih: ");
+                        pilih2 = Console.ReadLine();
+                        if (pilih2 == "S")
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Masukkan nominal setor: ");
+                            setoran = Convert.ToInt32(Console.ReadLine());
+                            nrek[i].setor(setoran);
+                        }
+                        else if (pilih2 == "T")
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Masukkan nominal tarik: ");
+                            tarikan = Convert.ToInt32(Console.ReadLine());
+                            nrek[i].tarik(tarikan);
+                        }
+                        else if (pilih2 == "X")
+                        {
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+                        if (pilih2 != "S" && pilih2 != "T" && pilih2 != "X" && pilih2 != urutrek[i])
+                        {
+                            Console.WriteLine("Invalid Input");
+                            Console.ReadKey();
+                        }
+                    }
+                }
+            }
+
+            if (pilih != "N" && pilih != "X" && pilih != "O")
+            {
+                Console.WriteLine("Invalid Input");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ W12/W12/REKENING.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+public class rekening
+{
+    public string nama;
+    public long saldo;
+    public long setoran;
+    public long tarikan;
+
+    public rekening (string nama, long saldo)
+    {
+        this.nama = nama;
+        this.saldo = saldo;
+    }
+    public void setor(long setoran)
+    {
+        saldo = saldo + setoran;
+        var x = new NumberFormatInfo { NumberGroupSeparator = "," };
+        Console.WriteLine("Berhasil setor Rp. " + setoran.ToString("n",x));
+        Console.ReadKey();
+        Console.Clear();   
+    }
+
+    public void tarik(long tarikan)
+    {
+        var x = new NumberFormatInfo { NumberGroupSeparator = "," };
+        if (tarikan > saldo)
+        {
+            Console.WriteLine("Penarikan Rp. " + tarikan.ToString("n", x) + " gagal. Saldo tidak cukup");
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine("Berhasil tarik Rp. " + tarikan.ToString("n", x));
+            saldo = saldo - tarikan;
+            Console.ReadKey();
+        }
+    }
+
+    public void ceksaldo()
+    {
+        var x = new NumberFormatInfo { NumberGroupSeparator = "," };
+        Console.WriteLine("Saldo:  Rp. " + (saldo.ToString("n", x)));
+    }
+}
